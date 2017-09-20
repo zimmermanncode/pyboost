3d8fe6860c2d27ee65de0a8147553e770a66ac4e
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-09-20 12:12:03 +0200

    Add Boost.Source class
    For downloading and building Boost source releases

---
+++ Boost/Source.py
@@ -0,0 +1,192 @@
+# C++Boost as a Python package
+#
+# Copyright (C) 2017 Stefan Zimmermann <user@zimmermann.co>
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Download and build Boost source releases with :class:`Boost.Source`.
+"""
+
+import platform
+import re
+import shutil
+import tarfile
+
+from bs4 import BeautifulSoup
+from furl import furl as URL
+from packaging.version import Version
+from path import Path
+from six import with_metaclass
+import requests
+import zetup
+
+__all__ = ('Source', )
+
+
+BITS = int(platform.architecture()[0].split('bit')[0])
+assert BITS in (32, 64)
+
+MSVC = platform.python_compiler().startswith('MSC')
+
+
+BOOST_URL = URL('http://www.boost.org')
+
+
+class Meta(zetup.meta):
+    """
+    Metaclass for :class:`Boost.Source`.
+
+    Provides class properties for retrieving info about available Boost
+    releases
+    """
+
+    @property
+    def RELEASE_URLS(cls):
+        """
+        ``dict`` of available Boost release versions and release page URLs.
+        """
+        response = requests.get(str(BOOST_URL) + '/users/history')
+        html = BeautifulSoup(response.text, 'html5lib')
+        return {Version(link.text.split()[1]):
+                URL(str(BOOST_URL) + link.get('href'))
+                for link in html.find_all('a')
+                if re.match(r'^Version\s+[0-9.]+$', link.text.strip())}
+
+    @property
+    def VERSIONS(cls):
+        """
+        ``list`` of available Boost release versions in ascending order.
+        """
+        return sorted(cls.RELEASE_URLS)
+
+    @property
+    def LATEST_VERSION(cls):
+        """
+        Latest available Boost release version.
+        """
+        return max(cls.RELEASE_URLS)
+
+    @property
+    def LATEST(cls):
+        """
+        :class:`Boost.Source` instance for latest Boost release version.
+        """
+        return Source(cls.LATEST_VERSION)
+
+
+class Source(with_metaclass(Meta, zetup.object)):
+    """
+    Handler for downloading and building a Boost source release.
+    """
+
+    def __init__(self, version, rootpath='.'):
+        """
+        Define absolute Boost source :attr:`.path` for given `version`.
+
+        Source path means directory of extracted Boost source archive under
+        `rootpath` (current directory by default), with naming scheme::
+
+           boost_major_minor_micro/
+        """
+        self.version = Version(str(version))
+        rootpath = Path(rootpath).realpath()
+        self.path = rootpath / 'boost_' + self.boost_lib_version
+
+    @property
+    def boost_lib_version(self):
+        """
+        Version string in ``BOOST_LIB_VERSION`` style.
+
+        :return: ``'major_minor_micro'``
+        """
+        return str(self.version).replace('.', '_')
+
+    @property
+    def archive(self):
+        """
+        Absolute path to ``.tar.bz2`` source archive.
+
+        The destination path of :meth:`.download`
+        """
+        return self.path + '.tar.bz2'
+
+    @property
+    def download_url(self):
+        """
+        Full URL to ``.tar.bz2`` archive.
+        """
+        response = requests.get(type(self).RELEASE_URLS[self.version])
+        html = BeautifulSoup(response.text, 'html5lib')
+        for link in html.find_all('a'):
+            if link.text.strip().endswith('.tar.bz2'):
+                return URL(link.get('href'))
+
+    def download(self):
+        """
+        Download ``.tar.bz2`` archive to parent directory of :attr:`.path`.
+        """
+        response = requests.get(self.download_url, stream=True)
+        with self.archive.open('wb') as file:
+            shutil.copyfileobj(response.raw, file)
+
+    def extract(self):
+        """
+        Extract downloaded ``.tar.bz2`` archive.
+        """
+        with tarfile.open(self.archive) as tar:
+            tar.extractall()
+
+    def bootstrap(self):
+        """
+        Run ``./bootstrap`` script in extracted Boost source release.
+
+        Auto-selects ``./bootstrap.bat`` for VisualC++ builds and
+        ``./bootstrap.sh`` for GCC
+        """
+        script = MSVC and ['booststrap.bat'] or ['bash', 'bootstrap.sh']
+        with self.path as cwd:
+            if zetup.call(script):
+                raise RuntimeError("Failed to run {!r} in {}"
+                                   .format(script, cwd))
+
+    def b2(self, args=None):
+        """
+        Run ``./b2`` binary in extracted Boost source release.
+
+        Auto-adds ``link=shared`` and ``address-model=32`` or ``=64`` to
+        (optionally) given `args` sequence
+        """
+        command = [Path('./b2'), 'link=shared',
+                   'address-model={}'.format(BITS)]
+        if args is not None:
+            command += args
+        with self.path as cwd:
+            if zetup.call(command):
+                raise RuntimeError("Failed to run {!r} in {}"
+                                   .format(command, cwd))
+
+    def build(self):
+        """
+        Build extracted Boost source release.
+        """
+        self.bootstrap()
+        self.b2()
+
+    def install(self, prefix=None):
+        """
+        Install Boost headers and built libs to current or `prefix` directory.
+        """
+        prefix = Path(prefix or '.').realpath()
+        self.b2(['install', '--prefix={}'.format(prefix)])
