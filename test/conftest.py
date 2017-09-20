1a302b7a8df45ba66877e69de5698e00e1d089af
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-09-20 16:10:28 +0200

    Update test fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -2,19 +2,40 @@ import re
 
 from bs4 import BeautifulSoup
 from packaging.version import Version
+from path import Path
 import pytest
 import requests
+
+import Boost
 
 
 @pytest.fixture
 def boost_release_links():
-    response = requests.get('http://www.boost.org/users/history')
+    history_url = 'http://www.boost.org/users/history'
+    response = requests.get(history_url)
     html = BeautifulSoup(response.text, 'html5lib')
-    links = []
-    for link in html.find_all('a'):
+    ilinks = iter(html.find_all('a'))
+    for link in ilinks:
         if re.match(r'^Version\s+[0-9.]+$', link.text.strip()):
-            links.append(link)
-    return links
+            release_link = link
+            break
+    else:
+        raise RuntimeError("no Boost release links found in {}"
+                               .format(history_url))
+
+    result = []
+    while release_link:
+        next_release_link = download_link = None
+        for link in ilinks:
+            if re.match(r'^Version\s+[0-9.]+$', link.text.strip()):
+                next_release_link = link
+                break
+            if re.match(r'^Download$', link.text.strip()):
+                download_link = link
+        if download_link:
+            result.append(release_link)
+        release_link = next_release_link
+    return result
 
 
 @pytest.fixture
@@ -23,3 +44,14 @@ def boost_versions(boost_release_links):
                              link.text.strip())
                     ['version'])
             for link in boost_release_links]
+
+
+@pytest.fixture
+def boost_source_rootpath():
+    return Path(__file__).realpath().dirname()
+
+
+@pytest.fixture
+def boost_test_sources(boost_source_rootpath, boost_versions):
+    return [Boost.Source(version, rootpath=boost_source_rootpath)
+            for version in (min(boost_versions), max(boost_versions))]
