7028db6cc4111cbbb80833dedcff8c80784f414d
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-10-08 00:57:53 +0200

    Extend test_Source

--- test/test_Source.py
+++ test/test_Source.py
@@ -1,18 +1,43 @@
 import re
+import platform
 
 from bs4 import BeautifulSoup
 from furl import furl
 from packaging.version import Version
 from path import Path
+import pytest
 import requests
 
-from Boost.Source import BOOST_URL, MIN_BOOST_VERSION
+from Boost.Source import BOOST_URL, MIN_BOOST_VERSION, MSVC, TOOLSET, _toolset
 from Boost import Source
 import Boost
 
 
+WIN = platform.system() == 'Windows'
+
+
 def test_import():
     assert Source is Boost.Source
+
+
+def test_unsupported_toolset(mocker):
+    mocker.patch('platform.python_compiler').return_value = 'UNSUPPORTED'
+    with pytest.raises(RuntimeError) as exc:
+        _toolset()
+    exc.match(repr('UNSUPPORTED'))
+
+
+def test_TOOLSET():
+    assert TOOLSET in ['gcc', 'msvc']
+    compiler = platform.python_compiler()
+    if TOOLSET == 'gcc':
+        assert compiler.startswith('GCC')
+    elif TOOLSET == 'msvc':
+        assert compiler.startswith('MSC')
+
+
+def test_MSVC():
+    assert MSVC is (TOOLSET == 'msvc')
 
 
 def test_BOOST_URL():
@@ -35,7 +60,7 @@ class TestMeta(object):
         assert Source.RELEASE_URLS == {
             Version(re.match(r'^Version\s+(?P<version>[0-9.]+)$',
                              link.text.strip())
-                    ['version']):
+                    .group('version')):
             furl('http://www.boost.org' + link.get('href'))
             for link in boost_release_links}
 
@@ -108,16 +133,51 @@ class TestSource(object):
 
     def test_download(self, boost_test_sources, boost_source_rootpath):
         for source in boost_test_sources:
-            source.archive.remove_p()
             archive = source.download()
             assert archive == Path(archive).realpath() == source.archive
             assert archive.isfile()
+            assert archive.dirname() == boost_source_rootpath
+
+    def test_extract_without_download(self, boost_test_sources):
+        for source in boost_test_sources:
+            with pytest.raises(FileNotFoundError) as exc:
+                source.extract()
+            exc.match(re.escape(repr(source.archive)))
 
     def test_extract(self, boost_test_sources, boost_source_rootpath):
         for source in boost_test_sources:
-            source.path.rmtree_p()
+            source.download()
             path = source.extract()
             assert path == Path(path).realpath() == source.path
             assert path.isdir()
+            assert path.dirname() == boost_source_rootpath
             for ext in ('.bat', '.sh'):
                 assert (path / 'bootstrap' + ext).isfile()
+
+    def test_b2_without_extract(self, boost_test_sources):
+        for source in boost_test_sources:
+            with pytest.raises(FileNotFoundError) as exc:
+                source.b2(['--help'])
+            exc.match(re.escape(repr(source.path)))
+
+    def test_b2_without_bootstrap(self, boost_test_sources):
+        for source in boost_test_sources:
+            source.download()
+            source.extract()
+            if TOOLSET == 'msvc':
+                with pytest.raises(RuntimeError):
+                    source.b2(['--help'])
+            else:
+                with pytest.raises(FileNotFoundError):
+                    source.b2(['--help'])
+
+    def test_bootstrap_and_b2(self, boost_test_sources):
+        for source in reversed(boost_test_sources):
+            source.download()
+            source.extract()
+            for ext in ('.exe', ''):
+                assert not (source.path / 'b2' + ext).exists()
+            source.bootstrap()
+            ext = WIN and '.exe' or ''
+            assert (source.path / 'b2' + ext).exists()
+            source.b2(['--help'])
