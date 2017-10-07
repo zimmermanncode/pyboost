1d64c98f553f715625b4a3c7d8f3455df0af9f1a
Stefan Zimmermann <zimmermann.code@gmail.com>
2017-10-08 00:56:23 +0200

    Add Boost.resolve, PREFIX, SOURCE, INCLUDE, LIB

--- Boost/__init__.py
+++ Boost/__init__.py
@@ -19,9 +19,65 @@
 C++Boost as a Python package.
 """
 
-from .Source import Source
+from path import Path
+
+from .Lib import Lib
+from .Source import Source, TOOLSET
 
 # __version__ module is created by setuptools_scm during setup
 from .__version__ import version as __version__
 
 __all__ = ('Source', )
+
+
+CAREFREE_HOME = (Path('~').expanduser() / '.carefree').mkdir_p()
+
+CAREFREE_BOOST_HOME = (CAREFREE_HOME / 'boost').mkdir_p()
+
+CAREFREE_BOOST_CACHE = (CAREFREE_BOOST_HOME / '.cache').mkdir_p()
+
+
+def resolve(version=None):
+    """
+    Search installed Boost and install if not found.
+
+    `version` defaults to latest available release
+
+    :return:
+        The prefix path of the Boost installation
+    """
+    source = Source(version, rootpath=CAREFREE_BOOST_CACHE)
+    prefix = CAREFREE_BOOST_HOME / source.boost_lib_version / TOOLSET
+    if prefix.exists():
+        return prefix, source
+
+    if source.path.exists():
+        print("Removing {!r}".format(source.path))
+        source.path.rmtree()
+    if source.archive.exists():
+        print("Using cached {!r}".format(source.archive))
+    else:
+        source.download()
+    source.extract()
+    source.build()
+    source.install(prefix=prefix.mkdir_p())
+    return prefix, source
+
+
+PREFIX, SOURCE = resolve()
+
+
+INCLUDE = [PREFIX / 'include']
+assert INCLUDE[0].isdir(), (
+    "Corrupted Boost installation! Missing directory {!r}. Please remove {!r}"
+    .format(INCLUDE[0], PREFIX))
+
+_boost_include = INCLUDE[0] / 'boost-' + SOURCE.boost_lib_version
+if _boost_include.isdir():
+    INCLUDE.append(_boost_include)
+
+
+LIB = Lib([PREFIX / 'lib'])
+assert LIB[0].isdir(), (
+    "Corrupted Boost installation! Missing directory {!r}. Please remove {!r}"
+    .format(LIB[0], PREFIX))
